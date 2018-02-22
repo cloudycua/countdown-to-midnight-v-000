e5f79ae65d8c1a7c59ceb0916dbbc5010915f9c2
Christine Cua <cloudycua@gmail.com>
2018-02-22 04:15:53 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,10 @@
 #write your code here
 
-def countdown
+def countdown(integer)
+  number = integer
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number -= number
+  end
+  return "HAPPY NEW YEAR!"
 end
